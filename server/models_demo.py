0a68c87dcbafde4def4176bd13754afa388889cd
Irina Stefan <istefan@arizona.edu>
2025-01-27 16:40:23 -0700

    Make re.match input raw string literals.

--- server/models_demo.py
+++ server/models_demo.py
@@ -503,7 +503,7 @@ class DemoUser(db.Model, Serializer):
     def validate_email(self, key, email):
         if not email:
             raise AssertionError("No email provided")
-        if not re.match("[^@]+@[^@]+\.[^@]+", email):
+        if not re.match(r"[^@]+@[^@]+\.[^@]+", email):
             raise AssertionError("Provided email is not an email address")
         return email
 
@@ -646,7 +646,7 @@ class DemoDomain(db.Model, Serializer):
     def validate_email_domain(self, key, email_domain):
         if not email_domain:
             raise AssertionError("No email_domain provided")
-        if not re.match("[^@]+\.[^@]+", email_domain):
+        if not re.match(r"[^@]+\.[^@]+", email_domain):
             raise AssertionError("Provided email_domain is not valid")
         return email_domain
 
